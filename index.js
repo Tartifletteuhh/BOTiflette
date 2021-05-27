4bcb2ad34f10ddafb3cf92828701fcbe18fe493a
Tartifletteuhh <claudon.nathanael@gmail.com>
2021-05-27 15:08:14 +0200

    27/05/21

--- index.js
+++ index.js
@@ -160,13 +160,30 @@ client.on('messageDelete', (message) => {
     
     const embed = new Discord.MessageEmbed()
         .setColor('#CF8523')
-        .setTitle('GhostPing détecté')
-        .setDescription(`Message :\n\n"${message.content}"`)
+        .setTitle('GhostPing détecté\n(message supprimé)')
+        .setDescription(`Message :\n"${message.content}"`)
         .addFields(
             {name: 'Channel', value: message.channel},
             {name: 'Auteur', value: message.author}
     )
     message.channel.send(embed)
+})
+client.on('messageUpdate', (oldMessage, newMessage) => {
+    if (!oldMessage.author || oldMessage.author.bot || oldMessage.guild.id === '623628341940977674') return
+
+    if (oldMessage.mentions.users.size === 0 && oldMessage.mentions.roles.size === 0 && !oldMessage.mentions.everyone) return
+
+    if(oldMessage.mentions.users.first() !== newMessage.mentions.users.first() || oldMessage.mentions.roles.first() !== newMessage.mentions.roles.first()) {
+        const embed = new Discord.MessageEmbed()
+            .setColor('#CF8523')
+            .setTitle('GhostPing détecté\n(message modifié)')
+            .setDescription(`Ancien message :\n"${oldMessage.content}"\nNouveau message :\n"${newMessage.content}"`)
+            .addFields(
+                {name: 'Channel', value: oldMessage.channel},
+                {name: 'Auteur', value: oldMessage.author}
+        )
+        oldMessage.channel.send(embed)
+    }
 })
 
 client.login(process.env.token)
