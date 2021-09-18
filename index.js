b94ad24e59203088348b405d8960d43895a4373a
Tartifletteuhh <claudon.nathanael@gmail.com>
2021-09-18 22:27:27 +0200

    temp raiisen 2

--- index.js
+++ index.js
@@ -31,33 +31,38 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-    try{
-        const args = message.content.trim().split(/ +/g)
-        const command = args.shift().toLowerCase()
-    
-        if (command == 'go' && message.mentions){
-    
-            let mentionbotiflette = message.mentions.members.first().id
+    if (message.author.id == 319929897021865985 || message.author.id == 418779371168464896) {
+        try{
+            const args = message.content.trim().split(/ +/g)
+            const command = args.shift().toLowerCase()
+        
+            if (command == 'go' && message.mentions.members){
+                
+                if(message.mentions.members.first().id == 735477050579615754) {
+
+                    const list = message.guild.members
+
+                    // find the role with the name "Community"
+                    let role = message.mentions.roles.first()
+                    //let role2 = message.guild.roles.find(r => r.name == 'TEST')
+        
+                    // if role doesn't exist, notify the author of command that the role couldn't be found
+                    if (!role) return message.channel.send(`**${message.author.username}**, role not found`)
+        
+                    // find all guild members that aren't bots, and add the "Community" role to each
+                    list.cache.forEach(member => member.roles.add(role)); 
+        
+                    // notify the author of the command that the role was successfully added to all members
+                    message.channel.send(`**${message.author.username}**, role **${role.name}** was added to all members`)
+                }
             
-            if(mentionbotiflette == 735477050579615754) {
-                // find the role with the name "Community"
-                let role = message.mentions.roles.first()
-                //let role2 = message.guild.roles.find(r => r.name == 'TEST')
-    
-                // if role doesn't exist, notify the author of command that the role couldn't be found
-                if (!role) return message.channel.send(`**${message.author.username}**, role not found`)
-    
-                // find all guild members that aren't bots, and add the "Community" role to each
-                message.guild.members.cache.forEach(member => member.roles.add(role));
-    
-                // notify the author of the command that the role was successfully added to all members
-                message.channel.send(`**${message.author.username}**, role **${role.name}** was added to all members`)
             }
-            
+        } catch (err) {
+            console.error(err)
+            message.channel.send('Il y a eu un problème.')
         }
-    } catch (err) {
-        console.error(err);
     }
+    
     
 })
 
